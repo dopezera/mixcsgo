67b780b9cc85629518ab96d37fa0c745bea16d34
patrickrbc <>
2021-06-06 13:00:10 -0300

    chore: update estlint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,28 @@
 module.exports = {
   env: {
-    node: true,
+    browser: true,
+    commonjs: true,
     es6: true,
     jest: true,
+    node: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    include: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['react', 'react-hooks'],
   rules: {
-    //"no-unused-vars": "off"
+    'react/prop-types': 0,
+    'no-console': 'off',
+    'no-control-regex': 0,
+    'no-unused-vars': 'off',
   },
 }
